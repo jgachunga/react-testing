20ef5c5abba93fd2b3743159a84ea71c003c574f
Gachunga <johnkachunga@gmail.com>
2022-03-08 03:31:38 +0300

    reducer

---
+++ src/reducers/posts/reducer.js
@@ -0,0 +1,11 @@
+import { types } from "../../actions/types";
+
+const postsReducer = (state=[], action) => {
+    switch(action.type) {
+        case types.GET_POSTS:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+export default postsReducer;
